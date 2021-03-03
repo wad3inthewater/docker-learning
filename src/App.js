f0c2544f52f8daaab85c4b510fc775c8b6553a84
wad3inthewater <wadefuller@Wades-Leaflink-Mac.local>
2021-03-02 22:19:16 -0500

    change text for feature branch flow

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload!
+          Lets push it to the limit.
         </p>
         <a
           className="App-link"
